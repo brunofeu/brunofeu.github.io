9b2c142902b34d810e20a324677e75d429ae0407
brunofeu <brunofeu89@gmail.com>
2021-12-29 16:54:46 -0300

    block scroll when open menu

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,17 @@
+import { useContext, useEffect } from 'react';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Main from './Components/Main';
+import PortfolioContext from './context/PortfolioContext';
 
 
 function App() {
+  const [menuOpen] = useContext(PortfolioContext);
+  
+  useEffect(() => {
+    const body = document.getElementsByTagName('body')[0]
+    menuOpen ? body.classList.add('blockSiteScrolling') : body.classList.remove('blockSiteScrolling')
+  }, [menuOpen])
   
   return (
   <body className=" w-11/12 xl:w-2/3 m-auto">
--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import PortfolioContext from '../context/PortfolioContext';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useContext(PortfolioContext)
+
+  const handleClick = (event) => {
+    event.classList.toggle('open')
+    !menuOpen ? setMenuOpen(true) : setMenuOpen(false)
+  }
 
   return (
     <header className="header-container">
@@ -8,15 +15,18 @@ function Header() {
         <h1 className="header-text-name">
           BRUNO FEU
         </h1>
-        <div className="header-nav-btn" onClick={ (e) => e.target.classList.toggle('open')  }>
-          <div className="header-btn-burger" onClick={ (e) => e.target.parentNode.classList.toggle('open')  }></div>
+        <div className="header-nav-btn" onClick={ (e) => handleClick(e.target)  }>
+          <div className="header-btn-burger" onClick={ (e) => handleClick(e.target.parentNode)  }></div>
         </div>
-        {/* <nav className="header-nav">
-          <a className="header-nav-text" href="#top">Home</a>
-          <a className="header-nav-text" href="#about">Sobre Mim</a>
-          <a className="header-nav-text" href="#projects">Projetos</a>
-          <a className="header-nav-text" href="#contato">Contato</a>
-        </nav> */}
+        {menuOpen && (
+          <nav className="header-nav">
+            <a className="header-nav-text" href="#top">Home</a>
+            <a className="header-nav-text" href="#about">Sobre Mim</a>
+            <a className="header-nav-text" href="#projects">Projetos</a>
+            <a className="header-nav-text" href="#contato">Contato</a>
+        </nav>
+        )
+      }
       </div>
     </header>
   )
