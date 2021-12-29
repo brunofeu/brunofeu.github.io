23174f5e1d59c6f2642a4a142613332598082798
brunofeu <brunofeu89@gmail.com>
2021-12-29 20:25:59 -0300

    projects css

--- portfolio/src/Components/ProjectCard.jsx
+++ portfolio/src/Components/ProjectCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function ProjectCard({project}) {
   return (
-    <div className=" flex justify-center items-center">
+    <div>
         <a href={project[1]}>
-          <img className="xl:brightness-90 justify-center m-1 rounded-xl max-h-80 h-2/3 hover:brightness-100 drop-shadow-lg" src={project[0]} alt="" />
+          <img className="project-card " src={project[0]} alt="" />
         </a>
     </div>
   )
