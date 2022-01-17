20b1cb571daa7a95b6a5546d86f747e6620ae201
brunofeu <brunofeu89@gmail.com>
2022-01-17 14:54:57 -0300

    adiciona borda na imagem, e altera cor do fundo

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -3,21 +3,25 @@ import profileImage from '../images/IMG_9726.jpg'
 
 function About() {
   return (
-    <div className="about-container" id='about'>
-      <section className="about-top-container">
+    <div id='about'>
+      <section className="about-container">
+        <div className="border-img">
+
         <img src={profileImage} alt="profile" className="about-picture" />
-        
-        <div className="about-top-text">
-          <h1>Olá! 👋</h1>
-          <h1>Eu sou o Bruno</h1>
+        </div>
+        <div className="about-text">
+          <h1>Olá, 👋 meu nome é 
+            <br />
+            <strong>Bruno</strong>
+          </h1>
           <p> Desenvolvedor Front-End, Engenheiro e <strong>apaixonado por tecnologia</strong>.</p>
         </div>
       </section>
-        <p className="about-quote"><em>“Seja curioso. Leia de tudo. Tente coisas novas. O que as pessoas chamam de inteligência se resume a curiosidade.”</em> — Aaron Swartz</p>
+        {/* <p className="about-quote"><em>“Seja curioso. Leia de tudo. Tente coisas novas. O que as pessoas chamam de inteligência se resume a curiosidade.”</em> — Aaron Swartz</p>
         <h3 className=" main-text-area">SOBRE MIM</h3>
         <p className="about-me-text">
         Atualmente, estou estudando desenvolvimento web na Trybe, uma escola que ensina a programar, a aprender e a trabalhar. Lá foi onde definitivamente encontrei algo que tenho prazer em fazer todos os dias. Gosto do que somos capazes de alcançar por meio do código. 
-        </p>
+        </p> */}
     </div>
   )
 }
